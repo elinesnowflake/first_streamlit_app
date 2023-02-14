ee16203df533e1e3682b2340f98003dd049d2dfd
elinesnowflake <125362751+elinesnowflake@users.noreply.github.com>
2023-02-14 12:34:40 +0100

    Update streamlit_app.py
    added fruit list for smooth section to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+""" DABW Lesson 2
+"""
 import streamlit
 
 streamlit.title('My Parents New Healhty Diner')
@@ -9,3 +11,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+""" DABW Lesson 3
+"""
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
