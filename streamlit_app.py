683df6b7f34408e57405821ec3e884b2fdd22878
elinesnowflake <125362751+elinesnowflake@users.noreply.github.com>
2023-03-08 13:45:30 +0100

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe.dataframe(my_data_rows)
 
-fruit_added = streamlit.text_input("What fruit would you like to add?", "")
+fruit_added = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', fruit_added)
 
 
