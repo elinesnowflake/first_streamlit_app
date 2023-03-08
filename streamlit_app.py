c4529eadc14c2847663aa546a8dba1aedf1c4f5a
elinesnowflake <125362751+elinesnowflake@users.noreply.github.com>
2023-03-08 13:43:45 +0100

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_added = streamlit.text_input("What fruit would you like to add?", list(my_fruit_list.index))
+fruit_added = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', fruit_added)
 
 
