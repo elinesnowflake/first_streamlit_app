8cd219aa84e166ec0e625b3350329d23462abff0
elinesnowflake <125362751+elinesnowflake@users.noreply.github.com>
2023-03-08 13:56:18 +0100

    Added expect try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 # DABW Lesson 2
 import streamlit
+import pandas 
+import requests
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healhty Diner')
 
@@ -12,7 +15,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # DABW Lesson 3
-import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,16 +27,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruitvice API resonse
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Pleasse select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Convert JSON data to a pandas data frame
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Convert to a streamlit data frame
-streamlit.dataframe(fruityvice_normalized)
+except:
+  streamlit.error()
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -46,6 +49,9 @@ streamlit.dataframe(my_data_rows)
 
 fruit_added = streamlit.text_input("What fruit would you like to add?")
 streamlit.write('Thanks for adding ', fruit_added)
+# my_cur.execute(f"insert into fruit_load_list values {fruit_added}")
+
+
 
 
 
